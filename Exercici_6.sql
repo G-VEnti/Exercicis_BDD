e9bb02f648b6ded9535c63531df388c217d6a9cb
G-VEnti <gerard.verdu@estudiant.enti.cat>
2025-11-09 09:35:05 +0000

    Exercici_6

---
+++ Exercici_6.sql
@@ -0,0 +1,47 @@
+CREATE DATABASE animals_exposats;
+
+USE animals_exposats;
+
+
+CREATE TABLE zoo
+(
+	id_zoo INT AUTO_INCREMENT,
+	nom VARCHAR(20),
+	ciutat VARCHAR(20),
+	pais VARCHAR(20),
+	pressupost DOUBLE,
+	grandaria DOUBLE CHECK (grandaria BETWEEN 1000 AND 1000000),
+	PRIMARY KEY (id_zoo)
+);
+
+
+CREATE TABLE especie
+(
+	id_especie INT AUTO_INCREMENT,
+	nom_vulgar VARCHAR(20),
+	nom_cientific VARCHAR(50),
+	familia VARCHAR(20),
+	en_perill_extincio BOOL,
+	PRIMARY KEY (id_especie)
+);
+
+
+CREATE TABLE animal
+(
+	id_animal INT AUTO_INCREMENT,
+	num_identificacio INT,
+	sexe BOOL,
+	any_naixement CHAR(4),
+	pais_origen VARCHAR(20),
+	continent VARCHAR(9),
+	id_zoo INT,
+	id_especie INT,
+	PRIMARY KEY (id_animal),
+	FOREIGN KEY (id_zoo) REFERENCES zoo(id_zoo),
+	FOREIGN KEY (id_especie) REFERENCES especie(id_especie)
+);
+
+
+DESCRIBE zoo;
+DESCRIBE especie;
+DESCRIBE animal;
---
+++ Exercici_6_borracio.sql
@@ -0,0 +1,5 @@
+DROP TABLE zoo, especie, animal;
+
+SHOW TABLES;
+
+DROP DATABASE animals_exposats;
