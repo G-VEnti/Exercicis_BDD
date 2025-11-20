9c12223c720e776b3b6629bc67d189ba1b89690f
G-VEnti <gerard.verdu@estudiant.enti.cat>
2025-11-20 12:52:29 +0000

    intser_arreglo

--- AA2_2_Exercicis/Exercici_4/mysqlinsertescola.sql
+++ AA2_2_Exercicis/Exercici_4/mysqlinsertescola.sql
@@ -1,19 +1,19 @@
-INSERT INTO alumne VALUES (1111AAA, Manolito, 222222222, 33/44/5555);
-INSERT INTO alumne VALUES (2222BBB, Ramon, 333333333, 44/55/6666);
-INSERT INTO alumne VALUES (3333CCC, Carlos, 444444444, 55/66/7777);
-INSERT INTO alumne VALUES (4444DDD, Ivan, 555555555,66/77/8888 );
-INSERT INTO alumne VALUES (5555EEE, Guillermo, 666666666, 77/88/99999);
+INSERT INTO alumne VALUES ('1111AAA', 'Manolito', '222222222', '3333/44/55');
+INSERT INTO alumne VALUES ('2222BBB', 'Ramon', '333333333', '4444/55/66');
+INSERT INTO alumne VALUES ('3333CCC', 'Carlos', '444444444', '5555/66/77');
+INSERT INTO alumne VALUES ('4444DDD', 'Ivan', '555555555', '6666/77/88');
+INSERT INTO alumne VALUES ('5555EEE', 'Guillermo', '666666666', '7777/88/99');
 
 
-INSERT INTO professor VALUES (66666, 77777777F, Alonso, Programador, 888888888);
-INSERT INTO professor VALUES (77777, 88888888G, David, Sistemes, 999999999);
-INSERT INTO professor VALUES (88888, 99999999H, Francisco, Informatica, 000000000);
-INSERT INTO professor VALUES (99999, 00000000I, Manuel, Art, 111111111);
-INSERT INTO professor VALUES (00000, 11111111J, Rosario, Mates, 222222222);
+INSERT INTO professor VALUES ('66666', '77777777F', 'Alonso', 'Programador', '888888888');
+INSERT INTO professor VALUES ('77777', '88888888G', 'David', 'Sistemes', '999999999');
+INSERT INTO professor VALUES ('88888', '99999999H', 'Francisco', 'Informatica', '000000000');
+INSERT INTO professor VALUES ('99999', '00000000I', 'Manuel', 'Art', '111111111');
+INSERT INTO professor VALUES ('00000', '11111111J', 'Rosario', 'Mates', '222222222');
 
 
-INSERT INTO assignatura VALUES (111111111, Programacio);
-INSERT INTO assignatura VALUES (222222222, Sistemacio);
-INSERT INTO assignatura VALUES (333333333, Informaticacio);
-INSERT INTO assignatura VALUES (444444444, Artacio);
-INSERT INTO assignatura VALUES (555555555, Matematicacio);
+INSERT INTO assignatura VALUES ('1111111111', 'Programacio');
+INSERT INTO assignatura VALUES ('2222222222', 'Sistemacio');
+INSERT INTO assignatura VALUES ('3333333333', 'Informaticacio');
+INSERT INTO assignatura VALUES ('4444444444', 'Artacio');
+INSERT INTO assignatura VALUES ('5555555555', 'Matematicacio');
