17aad8bb8dd136f912a967ec6a938903535ea418
G-VEnti <gerard.verdu@estudiant.enti.cat>
2025-11-20 13:13:59 +0000

    insert_funcional

--- AA2_2_Exercicis/Exercici_4/mysqlinsertescola.sql
+++ AA2_2_Exercicis/Exercici_4/mysqlinsertescola.sql
@@ -1,8 +1,8 @@
-INSERT INTO alumne VALUES ('1111AAA', 'Manolito', '222222222', '3333/44/55');
-INSERT INTO alumne VALUES ('2222BBB', 'Ramon', '333333333', '4444/55/66');
-INSERT INTO alumne VALUES ('3333CCC', 'Carlos', '444444444', '5555/66/77');
-INSERT INTO alumne VALUES ('4444DDD', 'Ivan', '555555555', '6666/77/88');
-INSERT INTO alumne VALUES ('5555EEE', 'Guillermo', '666666666', '7777/88/99');
+INSERT INTO alumne VALUES ('1111AAA', 'Manolito', '222222222', '2001/01/01');
+INSERT INTO alumne VALUES ('2222BBB', 'Ramon', '333333333', '2002/02/02');
+INSERT INTO alumne VALUES ('3333CCC', 'Carlos', '444444444', '2003/03/03');
+INSERT INTO alumne VALUES ('4444DDD', 'Ivan', '555555555', '2004/04/04');
+INSERT INTO alumne VALUES ('5555EEE', 'Guillermo', '666666666', '2005/05/05');
 
 
 INSERT INTO professor VALUES ('66666', '77777777F', 'Alonso', 'Programador', '888888888');
@@ -12,8 +12,21 @@ INSERT INTO professor VALUES ('99999', '00000000I', 'Manuel', 'Art', '111111111'
 INSERT INTO professor VALUES ('00000', '11111111J', 'Rosario', 'Mates', '222222222');
 
 
-INSERT INTO assignatura VALUES ('1111111111', 'Programacio');
-INSERT INTO assignatura VALUES ('2222222222', 'Sistemacio');
-INSERT INTO assignatura VALUES ('3333333333', 'Informaticacio');
-INSERT INTO assignatura VALUES ('4444444444', 'Artacio');
-INSERT INTO assignatura VALUES ('5555555555', 'Matematicacio');
+INSERT INTO assignatura VALUES ('1111111111', 'Programacio', '66666');
+INSERT INTO assignatura VALUES ('2222222222', 'Sistemacio', '77777');
+INSERT INTO assignatura VALUES ('3333333333', 'Informaticacio', '88888');
+INSERT INTO assignatura VALUES ('4444444444', 'Artacio', '99999');
+INSERT INTO assignatura VALUES ('5555555555', 'Matematicacio', '00000');
+
+
+INSERT INTO matriculacio VALUES ('2020', '1111AAA', '1111111111');
+INSERT INTO matriculacio VALUES ('2021', '2222BBB', '2222222222');
+INSERT INTO matriculacio VALUES ('2022', '3333CCC', '3333333333');
+INSERT INTO matriculacio VALUES ('2023', '4444DDD', '4444444444');
+INSERT INTO matriculacio VALUES ('2024', '5555EEE', '5555555555');
+
+
+SELECT * FROM alumne; 
+SELECT * FROM professor;
+SELECT * FROM assignatura;
+SELECT * FROM matriculacio;
